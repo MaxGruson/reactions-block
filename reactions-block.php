349fd3b1ead4710b08fe2f3b609127f320c54994
MaxGruson <max@gruson.studio>
2024-06-06 17:15:35 +0200

    Removed alingment options

--- reactions-block.php
+++ reactions-block.php
@@ -4,7 +4,7 @@
  * Description:       A block to display reactions and reviews by members of the audience and press. A reaction includes the author, publication, quote and link.
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            <a href="https://max.gruson.studio" target="_blank">Max Gruson</a>
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
