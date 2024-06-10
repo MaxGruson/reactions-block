a9059f41cffec4901d1f769cf52c42529d893ce8
MaxGruson <max@gruson.studio>
2024-06-10 15:56:35 +0200

    Fix empty save

--- build/reaction/render.php
+++ build/reaction/render.php
@@ -9,26 +9,32 @@
  * @package maxgruson/reactions-block
  */
 
-$author      = $attributes['author'] ?? '';
-$publication = $attributes['publication'] ?? '';
-$quote       = $attributes['quote'] ?? '';
-$link        = $attributes['link'] ?? '';
+$author        = $attributes['author'] ?? '';
+$publication   = $attributes['publication'] ?? '';
+$quote         = $attributes['quote'] ?? '';
+$reaction_link = $attributes['link'] ?? '';
 ?>
 <li <?php echo get_block_wrapper_attributes( array( 'class' => 'reactions-block__reaction' ) ); ?>>
 	<figure>
-		<?php if ( ! empty( $link ) ) { ?>
-		<a target="_blank" href="<?php echo esc_url( $link['url'] ); ?>">
+		<?php if ( ! empty( $reaction_link ) ) { ?>
+		<a target="_blank" href="<?php echo esc_url( $reaction_link['url'] ); ?>">
 		<?php } else { ?>
 		<div>
 		<?php } ?>
-			<blockquote cite="<?php echo esc_url( $link['url'] ); ?>">
+			<blockquote cite="
+				<?php
+				if ( ! empty( $reaction_link ) ) {
+					echo esc_url( $reaction_link['url'] );
+				}
+				?>
+			">
 				<p><?php echo wp_kses_post( $quote ); ?></p>
 			</blockquote>
 			<figcaption>
 				&mdash; <span><?php echo wp_kses_post( $author ); ?></span>, 
 				<cite><?php echo wp_kses_post( $publication ); ?></cite><span class="link-arrow"><?php echo ( isset( $link['url'] ) ? '&#8599;' : '' ); ?></span>
 			</figcaption>
-		<?php if ( ! empty( $link ) ) { ?>
+		<?php if ( ! empty( $reaction_link ) ) { ?>
 		</a>
 		<?php } else { ?>
 		</div>
